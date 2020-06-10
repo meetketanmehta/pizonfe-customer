097e7ea65948ae300cefd772be289034eca10162
Meet Mehta <meet.1@iitj.ac.in>
2020-06-10 19:45:20 +0530

    Changed categories endpoint

--- lib/res/values/EndPoints.dart
+++ lib/res/values/EndPoints.dart
@@ -1,6 +1,6 @@
 class EndPoints {
   static final String MAPS_API_KEY = "AIzaSyBHyrf7slSxrVvi8pqXOC6Ij161h35uF8o";
-  static final String categories = "https://3tad1tsoe2.execute-api.us-east-1.amazonaws.com/dev/categories";
+  static final String categories = "https://9z7rj2idm7.execute-api.us-east-1.amazonaws.com/dev/categories";
   static final String login = "https://t3fpsp0ky8.execute-api.us-east-1.amazonaws.com/dev/login";
   static final String register = "https://t3fpsp0ky8.execute-api.us-east-1.amazonaws.com/dev/register";
   static final String addresses = "https://t3fpsp0ky8.execute-api.us-east-1.amazonaws.com/dev/address";
